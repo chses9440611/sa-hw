acb486b6c8e152c0d9dfce0c8b518f0ee69d3f61
chses910372 <chses910372@gmail.com>
2018-09-30 16:50:00 +0800

    finish hw1-1

---
+++ hw1/q1.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+ls -lSAR |grep "^[d-]" | tr -s ' ' ' ' | cut  -d' ' -f1,5,9 | sort -rnk 2 | awk 'BEGIN{ total=0;dir_num=0;file_num=0}{ printf "%2d: %-15s %s\n", NR, $2, $3; ((total+=$2));} /^d/{dir_num++} /^-/{file_num++}END{print "File num: ", file_num, "\nDir num: ", dir_num, "\nTotal: ", total}' | grep --color=never "^[TDF ][^6-9]"
